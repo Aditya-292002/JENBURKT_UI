ae0466cb9249041f78bad7399356d7041c7c38d1
hemantComflex <hemant.pade@comflextech.com>
2025-06-27 14:07:55 +0530

    EDIT INVOICE

--- src/app/Forms/sample-received/sample-received.component.ts
+++ src/app/Forms/sample-received/sample-received.component.ts
@@ -53,9 +53,9 @@ export class SampleReceivedComponent implements OnInit {
   GETSAMPLERECEIVEDETAILSBYUSERID() {
     let data = {
       "USER_ID": this.userInfo.USER_ID,
+      "SALES_ROLE_ID":this.userInfo.SALESROLE_ID,
       "FROM_DATE":this.FROM_DATE,
       "TO_DATE":this.TO_DATE,
-      "SALES_ROLE_ID":this.salesRoleId,
       "HQ_CODE":this.HQ_CODE,
       "STATUS":this.STATUS
     }
