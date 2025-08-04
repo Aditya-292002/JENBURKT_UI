7f38234f2ecfd8a9361feb1f4dfe8d12e3de0ce4
hemantComflex <hemant.pade@comflextech.com>
2025-08-04 18:32:59 +0530

    document approval changes

--- src/app/Forms/document-pmt-approval/document-pmt-approval.component.ts
+++ src/app/Forms/document-pmt-approval/document-pmt-approval.component.ts
@@ -10,55 +10,57 @@ import { URLService } from 'src/app/Service/url.service';
   styleUrls: ['./document-pmt-approval.component.css']
 })
 export class DocumentPmtApprovalComponent implements OnInit {
-  isHighLightUnit: string = "No";
-  isHighLightCycle: string = "No";
-  isLoaded: boolean = false;
-  userInfo: any
-  CME_LIST: any = []
-  USER_LIST: any = []
-  CME_CODE: any;
-  SUBMITTED_ON = new Date();
-  USER_NAME: any;
-  constructor(private authService: AuthService, private url: URLService, private http: HttpService, private toastrService: ToastrService,) { }
-
+  isHighLightUnit:string="No";
+  isHighLightCycle:string="No";
+  isLoaded:boolean=false;
+  userInfo:any
+  CME_LIST:any=[]
+  USER_LIST:any=[]
+  CME_CODE:any;
+  SUBMITTED_ON=new Date();
+  USER_NAME:any;
+   constructor(private authService: AuthService, private url: URLService, private http: HttpService, private toastrService: ToastrService,) { }
+ 
   ngOnInit(): void {
-    this.userInfo = this.authService.getUserDetail();
-    this.getDocumentMasterList();
+     this.userInfo = this.authService.getUserDetail();
+      this.getDocumentMasterList();
   }
-  getDocumentMasterList() {
-    this.userInfo = this.authService.getUserDetail();
+   getDocumentMasterList(){
+   this.userInfo = this.authService.getUserDetail();
 
     let data = {}
-    this.isLoaded = true;
+     this.isLoaded = true;
     this.http.postnew(this.url.GETDOCUMENTAPPROVALMASTERLIST, data).then(
       (res: any) => {
-        console.log(res, 'res master list');
-
-        this.isLoaded = false;
+        console.log(res,'res master list');
+        
+       this.isLoaded = false;
         this.CME_LIST = res.CME_LIST;
         this.USER_LIST = res.USER_LIST;
-
+        
       },
       error => {
         console.log(error);
         this.toastrService.error("Oops, Something went wrong.");
       }
     );
+ }
+
+ saveDocumentApproval(){
+  let data={
+    "CME_NO":this.CME_CODE,
+    "DOC_SUBMITTED_ON":this.SUBMITTED_ON,
+    "USER_ID":this.USER_NAME
   }
 
-  saveDocumentApproval() {
-    let data = {
-      "CME_NO": this.CME_CODE,
-      "DOC_SUBMITTED_ON": this.SUBMITTED_ON,
-      "USER_ID": this.USER_NAME
-    }
-
-    console.log('USER_ID', data);
-    this.isLoaded = true;
+  console.log('USER_ID',data);
+   this.isLoaded = true;
     this.http.postnew(this.url.SAVEDOCUMENTAPPROVAL, data).then(
-      (res: any) => {
+      (res: any) => { 
+       
+                if (res.FLAG == true) {
 
-        if (res.FLAG == true) {
+         
           this.isLoaded = false;
           this.toastrService.success(res.MSG);
         } else if (res.FLAG == false) {
@@ -70,6 +72,6 @@ export class DocumentPmtApprovalComponent implements OnInit {
         this.toastrService.error("Oops, Something went wrong.");
       }
     );
-  }
+ }
 
 }
