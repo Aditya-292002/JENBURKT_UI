8a994c732e4d6d1f89750c7fc7ec470816945871
hemantComflex <hemant.pade@comflextech.com>
2025-08-22 10:43:37 +0530

    moe view minor change

--- src/app/Forms/moe-report-view/moe-report-view.component.ts
+++ src/app/Forms/moe-report-view/moe-report-view.component.ts
@@ -17,7 +17,7 @@ export class MoeReportViewComponent implements OnInit {
   periodList:any = [];
   isLoaded:boolean=false;
   roleList: any=[];
-  AREA_CODE:any=[];
+  AREA_CODE:any;
   roleListData: any=[];
   pdfSrc: any;
   pdfSrcflag: boolean=false;
@@ -101,20 +101,27 @@ export class MoeReportViewComponent implements OnInit {
     this.isLoaded=true;
         this.http.postnew(this.url.ConvertPdfToBase64, data).then(
       (res:any)=>{
-        
-        console.log('RES',res);
-         this.isLoaded=false;
-        this.pdfSrcflag=true;
-  this.AREA_CODE=''
-          this.pdfSrc=`data:application/pdf;base64,${res.Base64Pdf}`
-          const base64Data =this.pdfSrc
-          console.log('periodList',this.pdfSrc);
-          const fileName = res.FileName;
-          const link = document.createElement('a');
-          link.href = this.pdfSrc;
-         // link.download = fileName; // 👈 custom file name here
-          //link.click();
-          this.pdfSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.pdfSrc);
+        if(res.flag==true){
+
+          this.isLoaded=false;
+         this.pdfSrcflag=true;
+          //this.AREA_CODE=''
+           this.pdfSrc=`data:application/pdf;base64,${res.Base64Pdf}`
+           const base64Data =this.pdfSrc
+           console.log('periodList',this.pdfSrc);
+           const fileName = res.FileName;
+           const link = document.createElement('a');
+           link.href = this.pdfSrc;
+          // link.download = fileName; // 👈 custom file name here
+           //link.click();
+           this.pdfSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.pdfSrc);
+         //  this.toastrService.success(res.Message);
+        }
+        else if(res.flag==false){
+            this.isLoaded=false;
+            this.toastrService.error(res.Message);
+        }
+       // console.log('RES',res);
       },
       error =>{
          this.isLoaded=false;
